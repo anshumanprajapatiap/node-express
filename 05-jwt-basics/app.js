ee3724ae1102932464513c6933e97a08dc669b5a
anshumanprajapati <anshumanprajapati2000@gmail.com>
2023-12-29 14:26:34 +0530

    jwt setup

--- 05-jwt-basics/app.js
+++ 05-jwt-basics/app.js
@@ -1,10 +1,34 @@
 console.log('JWT Application start')
+require('dotenv').config()
+require('express-async-errors');
 
 const express = require('express')
 const app = express()
 
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
-const port = 5001;
-app.listen(port, ()=>{
-    console.log(`Server Listening on port ${port}...`)
-})
+
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+
+const port =  process.env.PORT || 5001;
+const start = async () => {
+
+    try{
+        const connectingString = `${process.env.method}://${process.env.dbHost}:${process.env.password}@${process.env.projectName}.${process.env.instanceName}/${process.env.dbName}?retryWrites=true&w=majority`;
+        // await connectDB(connectingString);
+        app.listen(port, ()=>{
+            console.log(`Server is listening on port ${port}....`)
+        })
+    }catch(err){
+        console.log(`Error ${err}`)
+    }
+}
+
+start();
