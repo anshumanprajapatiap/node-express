6d249c9d39569903603478a425cae24232b43783
anshumanprajapati <anshumanprajapati2000@gmail.com>
2024-01-01 01:00:24 +0530

    JWT Basics complete

--- 05-jwt-basics/app.js
+++ 05-jwt-basics/app.js
@@ -5,6 +5,7 @@ require('express-async-errors');
 const express = require('express')
 const app = express()
 
+const mainRouter = require('./routes/main');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
@@ -12,6 +13,9 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 // middleware
 app.use(express.static('./public'));
 app.use(express.json());
+
+
+app.use('/api/v1', mainRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
